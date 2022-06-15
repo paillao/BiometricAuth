01d918b1b88ca70fdb3612457acb11eb979323fd
paillao <paillao@gmail.com>
2022-06-14 20:58:55 -0400

    Initial Commit

---
+++ BiometricAuth/BiometricAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiometricAuthApp.swift
+//  BiometricAuth
+//
+//  Created by Jorge Luis Paillao on 14-06-22.
+//
+
+import SwiftUI
+
+@main
+struct BiometricAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BiometricAuth/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BiometricAuth
+//
+//  Created by Jorge Luis Paillao on 14-06-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
