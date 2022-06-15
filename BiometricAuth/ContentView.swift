a1daae01cecd773627ed9734529f2f3c408f76dc
paillao <paillao@gmail.com>
2022-06-15 09:47:03 -0400

    SwiftUI Face ID Autentication App OK

--- BiometricAuth/ContentView.swift
+++ BiometricAuth/ContentView.swift
@@ -6,12 +6,46 @@
 //
 
 import SwiftUI
+import LocalAuthentication
 
 struct ContentView: View {
+    
+    @State private var auth = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Text(auth ? "estas autenticado" : "Necesitas Autenticarte").font(.title).bold()
+            Button(auth ? "Cerrar" : "Autenticar") {
+                if auth {
+                    auth = false
+                } else{
+                    autentication()
+                }
+                
+            }.buttonStyle(.borderedProminent)
+                .buttonBorderShape(.roundedRectangle)
+                .accentColor(.black)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(auth ? Color.green : Color.red)
     }
+    
+    private func autentication(){
+        var error: NSError?
+        
+        let laContext = LAContext()
+        if
+            laContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
+            laContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Autentícate usando el sensor Biométrico") { autenticated, error in
+                if autenticated{
+                    auth = true
+                }
+            }
+        } else{
+            auth = true
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
